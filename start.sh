f28c5b10b5646382dc660b41d8d2ac002280c0e0
NexoHost Corporation <96266038+NexoHost@users.noreply.github.com>
2024-08-09 01:21:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
-echo "⚙️ Versão do Script: 1.9"
+echo "⚙️ Versión del Script: 1.9"
 echo "🛠 Iniciando PHP-FPM..."
 /usr/sbin/php-fpm --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "🛠 Iniciando Nginx..."
-echo "✅ Inicializado com sucesso"
+echo "✅ Inicializado con éxito"
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
